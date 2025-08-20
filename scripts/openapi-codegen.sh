c0dac420924f216225e69dacbba2d4f7d879d829
Erik Karlsson <erikdkarlsson8@gmail.com>
2025-08-20 00:09:17 -0700

    Codegen works. Need to regen a bunch of stuff and make sure I don't delete anything important.

--- scripts/openapi-codegen.sh
+++ scripts/openapi-codegen.sh
@@ -1,29 +1,46 @@
 #/usr/bin/env bash
 
-COMMAND=$1
-
 PACKAGE_VERSION=1.0.0
 PROJECT_NAME=tfd-api-client
 
 # Available generators: python, python-pydantic-v1, python-fastapi, go, go-server.
 OPENAPI_GENERATOR=python
 OPENAPI_CONFIG=openapi-codegen.yaml
+OPENAPI_PREPROCESS=scripts/preprocess_openapi.py
 
-NEXON_SCHEMA_DIR=spec
-NEXON_OPENAPI_SCHEMA=tfd.openapi.yaml
+SPEC_DIR=spec
+OPENAPI_SPEC=tfd.openapi.yaml
+OPENAPI_FILE=$SPEC_DIR/$OPENAPI_SPEC
 
-if [ -z $COMMAND ]; then
-  echo "openapi-gen ${OPENAPI_GENERATOR} ${NEXON_OPENAPI_SCHEMA}"
+preprocess() {
+  python $OPENAPI_PREPROCESS
+  mv -v ${OPENAPI_FILE} ${OPENAPI_FILE}.bu
+  mv -v out/staged-openapi.yaml $SPEC_DIR/$OPENAPI_SPEC
+}
+
+codegen() {
+  echo "openapi-gen ${OPENAPI_GENERATOR} ${OPENAPI_SPEC}"
   docker run --rm \
     -v ${PWD}:/local openapitools/openapi-generator-cli generate \
-    -i /local/${NEXON_SCHEMA_DIR}/${NEXON_OPENAPI_SCHEMA} \
+    -i /local/${OPENAPI_FILE} \
     -c /local/${OPENAPI_CONFIG} \
     -p ${PACKAGE_VERSION} \
     -g ${OPENAPI_GENERATOR} \
-    -o /local/out
-    # -o /local/out/${PROJECT_NAME}
-else
-  echo "run with cmds: ${@}"
-  docker run --rm \
-    -v ${PWD}:/local openapitools/openapi-generator-cli $@
-fi
+    -o /local/out/${OPENAPI_GENERATOR}-client
+}
+
+case "$1" in
+  pre)
+    echo "Running preprocessing"
+    preprocess
+    ;;
+  gen)
+    echo "Running default codegen"
+    codegen
+    ;;
+  *)
+    echo "run with cmds: ${1}"
+    docker run --rm \
+      -v ${PWD}:/local openapitools/openapi-generator-cli $@
+    ;;
+esac
