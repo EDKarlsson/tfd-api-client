b771bb9bd01f53864e08a6db230743b45fe52ad2
Erik Karlsson <erikdkarlsson8@gmail.com>
2025-08-19 23:27:30 -0700

    Finished the preprocessing of path and schema objects in openapi. Working on codegen next and testing.

--- scripts/preprocess_openapi.py
+++ scripts/preprocess_openapi.py
@@ -3,10 +3,10 @@ import argparse
 import re
 import logging
 import logging.config
-import os
 from pathlib import Path
+from typing import Callable
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("preprocessor")
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
 # Example usage:
@@ -14,16 +14,12 @@ SPEC_DIR = PROJECT_ROOT / "spec"
 CONFIG_DIR = PROJECT_ROOT / "configs"
 
 
-def ymlprint(ymlobj):
-    print(yaml.dump(ymlobj))
-
-
-def preprocess(spec_file, conf_file):
+def process(spec_file: str, conf_file: str) -> dict:
     with open(conf_file, "r") as cf, open(spec_file, "r") as mds:
         config_yml = yaml.load(cf, Loader=yaml.FullLoader)
         openapi_yml = yaml.load(mds, Loader=yaml.FullLoader)
 
-    openapi_spec = process_paths(openapi_yml, config_yml)
+    openapi_spec = process_api(openapi_yml, config_yml)
     return openapi_spec
 
 
@@ -36,75 +32,89 @@ def simplify_path(path_str: str) -> str:
     )
 
 
-def format_operation_id(path_str: str, make_plural: bool) -> str:
+def make_plural(instr: str) -> str:
+    return instr + "s" if instr[-1] != "s" else instr
+
+
+def format_operation_id(path_str: str, plural: bool) -> str:
     operation_id = re.sub(r"[-/]", " ", path_str)
     operation_id = "get" + operation_id.title().replace(" ", "")
-    if make_plural and operation_id[-1] != "s":
-        operation_id += "s"
+    operation_id = make_plural(operation_id) if plural else operation_id
     return operation_id
 
 
-def get_schema_ref(response_ref) -> str:
+def get_schema_ref(response_ref: dict) -> str:
     return response_ref["get"]["responses"]["200"]["content"]["application/json"][
         "schema"
     ]["$ref"]
 
 
-def set_schema_ref(path_obj, ref_string):
+def set_schema_ref(path_obj: dict, reference:str)->dict:
     path_obj["get"]["responses"]["200"]["content"]["application/json"]["schema"][
         "$ref"
-    ] = ref_string
+    ] = reference
     return path_obj
 
 
-def format_response(response_str: str, make_plural: bool = True) -> str:
-    plural = "s" if make_plural else ""
-    return response_str.replace("Response", plural)
+def format_response(response_str: str, plural: bool = True) -> str:
+    suffix = "s" if plural else ""
+    return response_str.replace("Response", suffix)
 
 
-def process_paths(openapi_spec, config):
+def regex_flag(
+    re_method: Callable[[str, str], bool], config: dict, option: str, string: str
+) -> bool:
+    return True if re_method("|".join(config[option]), string) else False
+
+
+def search_flag(config: dict, option: str, string: str) -> bool:
+    return regex_flag(re.search, config, option, string)
+
+
+def match_flag(config: dict, option: str, string: str) -> bool:
+    return regex_flag(re.match, config, option, string)
+
+
+def process_api(openapi_spec: dict, config: dict)->dict:
     api_paths = openapi_spec["paths"]
-    custom = config["paths"]
+    conf_path = config["paths"]
+    conf_general = config["general"]
+    conf_schemas = config["schemas"]
     for k in list(api_paths.keys()):
         # Changes path from /foo/bar/{baz}/boo[.json] to bar/boo[.json]
         path = simplify_path(k)
         logger.debug(f"path: {path}")
 
-        has_op_id = api_paths[k]["get"].get("operationId", None)
-        if not has_op_id:
-            logger.info("missing op id")
-            if path in custom.keys():
-                operationId = format_operation_id(custom[path], make_plural=False)
-            else:
-                if path.split("/")[0] in config["general"]["set-plural"]:
-                    operationId = format_operation_id(path, make_plural=True)
-                else:
-                    operationId = format_operation_id(path, make_plural=False)
-
-            logger.info(f"Set operationId->{operationId}")
+        if path in conf_path:
+            operationId = format_operation_id(conf_path[path], plural=False)
+        else:
+            plural_flag = search_flag(conf_general, "set-plural", path)
+            operationId = format_operation_id(path, plural=plural_flag)
+            logger.debug(f"Set operationId -> {operationId}")
             api_paths[k]["get"]["operationId"] = operationId
 
-        response_ref = format_response(get_schema_ref(api_paths[k]))
-        logger.debug(f"Set $ref->{response_ref}\n")
+        schema_ref = get_schema_ref(api_paths[k])
+        old_component = schema_ref.split("/")[-1]
+        plural_flag = not match_flag(conf_schemas, "skip", old_component)
+        response_ref = format_response(schema_ref, plural_flag)
+        new_component = response_ref.split("/")[-1]
+
+        # Skip any
+        # if not match_flag(conf_schemas, "skip", old_component):
+        if old_component != new_component:
+
+            openapi_spec["components"]["schemas"][new_component] = openapi_spec[
+                "components"
+            ]["schemas"].pop(old_component)
+
+        logger.debug(f"Component old,new: {old_component} -> {new_component}")
+        logger.debug(f"Set $ref -> {response_ref}\n")
+
         openapi_spec["paths"][k] = set_schema_ref(api_paths[k], response_ref)
-
     return openapi_spec
-
-
-def process_schemas(openapi_spec, config):
-    schemas_prop = openapi_spec["components"]["schemas"]
-    for k in list(schemas_prop.keys()):
-        nk = config["metadata"].get(k, None)
-        if nk:
-            schemas_prop[nk] = schemas_prop.pop(k)
-    openapi_spec["components"]["schemas"] = schemas_prop
-    print(f"Updated {len(schemas_prop.keys())} of keys...")
-    return openapi_spec
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    # parser.add_argument("target", choices=["datamodel","openapi"], help="Select to preprocess spec file for either Pydantics datamodel codegen, or openapi codegen for client.")
     parser.add_argument(
         "--dry",
         action="store_true",
@@ -117,20 +127,30 @@ if __name__ == "__main__":
         "--spec", "-s", help="OpenAPI Specification File", default="tfd.openapi.yaml"
     )
     parser.add_argument(
-        "--log-config", help="Set python logging config", default="logging.conf"
+        "--verbose", "-v", help="display debug information", action="store_true"
     )
-
+    parser.add_argument(
+        # "--log-config", help="Set python logging config", default="logging.conf"
+        "--log-config",
+        help="Set python logging config",
+        default="logging.yaml",
+    )
     args = parser.parse_args()
+
+    with open(f"{CONFIG_DIR}/{args.log_config}", "rt") as lcf:
+        log_conf = yaml.safe_load(lcf.read())
+        logging.config.dictConfig(log_conf)
+    if args.verbose:
+        logger.info("Verbose Output")
+        logger.setLevel(logging.DEBUG)
 
     conf_file = f"{CONFIG_DIR}/{args.config}"
     spec_file = f"{SPEC_DIR}/{args.spec}"
 
-    logging.config.fileConfig(f"{CONFIG_DIR}/{args.log_config}")
-
-    openapi_spec = preprocess(spec_file=spec_file, conf_file=conf_file)
+    openapi_spec = process(spec_file=spec_file, conf_file=conf_file)
 
     if not args.dry:
         with open("./out/staged-schemas.openapi.yaml", "w") as md_fyml:
-            yaml.dump(openapi_spec, md_fyml)
+            yaml.dump(openapi_spec, md_fyml, sort_keys=False)
     # else:
     #     print(yaml.dump(openapi_spec))
